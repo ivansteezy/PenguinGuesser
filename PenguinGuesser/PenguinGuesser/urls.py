772e88f12f61ea26a36850adf7c1fd6e93103df6
Ivan Ayala <network.9961@gmail.com>
2021-11-18 22:16:56 -0600

    adding homepage

--- PenguinGuesser/PenguinGuesser/urls.py
+++ PenguinGuesser/PenguinGuesser/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import re_path
+
+from Guesser.views import index
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    re_path(r'^$', index, name='index')
 ]
